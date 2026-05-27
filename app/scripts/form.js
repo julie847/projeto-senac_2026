5d13e9582dae76f9030199f7823fc816d7c14a12
JULIE DE ANDRADE <60157665062@senacrs.edu.br>
2026-05-27 17:14:41 -0300

    arrumei o crie seu blog

--- app/scripts/form.js
+++ app/scripts/form.js
@@ -1,29 +1,38 @@
-const form = document.getElementById ('post-form');
+import { openStoryModal } from "./modal.js"
 
-form.addEventListener ('submit', function (event) {
+const form = document.getElementById('post-form');
 
-    event.preventDefault();
-    
-    const form_data = new FormData (event.target)
+form.addEventListener('submit', function(event) {
 
-    const autor = form_data.get ('titulo')
-    const titulo = form_data.get ('titulo')
-    const email = form_data.get ('email')
-    const historia = form_data.get ('historia')
+        event.preventDefault();
 
-    if (!autor || !historia) { //verificação de campos vazios
-        alert("Os campos 'autor' e 'historia' são obrigatórios!")
-        return
-    }
+        const form_data = new FormData(event.target)
 
-    const article = document.createElement('article');
-    article.className = "artigo";
+        const autor = form_data.get('autor')
+        const titulo = form_data.get('titulo')
+        const email = form_data.get('email')
+        const historia = form_data.get('historia')
+
+        if (!autor || !historia) { //verificação de campos vazios
+                alert("Os campos `Autor` e `História` são obrigatórios!")
+                return
+        }
+
+        const article = document.createElement('article');
+        article.className = "artigo";
+
+        article.innerHTML =
+                `<h3>${titulo}</h3>
+                <p><strong> Autor: ${autor} </strong></p>
+                <p><small> Email: ${email} </small></p>
+                <p>${historia.replace('/\n/g', '<br />')}</p>
+                <hr />`;
+
+        article.addEventListener('click', function() {
+                openStoryModal(titulo, autor, historia);
+        })
+
+        document.getElementById('historias').appendChild(article)
+
+        event.target.reset()
 })
-    article.innerHTML = 
-    `<h3>${titulo}</h3>
-    <p><strong> Autor:{email} </small></p>
-    <p>${historia.replace(`/\n/g`, '<br />')}</p>
-    <hr />`;
-
-    document.getElementById('historias').appendChild(article)
-    Event.target.reset ()
