621b1231bb5afa7478a9f67a0b16cf57f648c456
JULIE DE ANDRADE <60157665062@senacrs.edu.br>
2026-06-01 17:25:25 -0300

    exercicios aula 01/06

---
+++ app/style/proj/main.py
@@ -0,0 +1,17 @@
+###########################  exercicios aula 1/6  ########################### 
+
+
+def calcula_area_triangulo (base: float, altura: float):
+    area = (base * altura) /2
+    print (F"A area do triângulo é: {area}")
+
+
+    def converte_temperatura_fahrenreit(graus_celsius: float):
+         celsius = (fahrenreit - 32) * 5/9
+        temperatura = (graus_celsisus * (9/5)) - 32
+        print (F" A temperatura em fahrenreit é {temperatura}")
+
+
+        def converte_em_dolares (reais: float):
+            dolares = reais / 5.04
+            print (f"O valor em dólarers é: {dolares}")
