9a4e7978c9b54b84a8cb2b41e9405648e39ff7a4
JULIE DE ANDRADE <60157665062@senacrs.edu.br>
2026-06-02 17:25:22 -0300

    aula 02/06 exercicos

--- app/style/proj/main.py
+++ app/style/proj/main.py
@@ -15,3 +15,23 @@ def calcula_area_triangulo (base: float, altura: float):
         def converte_em_dolares (reais: float):
             dolares = reais / 5.04
             print (f"O valor em dólarers é: {dolares}")
+
+ def calcula_salario_apos_desconto ( salario: float ):
+     salario_resultante = salario - (salario * (15/100))
+          return salario_resultante
+          ############################## Exercicos aula 02/06 ##########################
+
+          def classificar_pop(idade: int) -> str:
+
+         if idade <12:
+            return "Criança"
+
+         elif idade >12 and idade < 18:
+             return "adolescente"
+
+             elif idade >= 18 and idade <=60
+             return "adulto"
+
+         else:    ''
+             return "idoso"
+
